bf29bd8ced217e19b0d0afb590c20566f66576eb
Sohamdp10 <cseaimlbatch28@gmail.com>
2026-06-13 18:03:35 +0530

    Create string compression.cpp

---
+++ WEEK 2/Day 5/string compression.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0;  // writing position
+        int read = 0;   // reading position
+
+        while (read < chars.size()) {
+            char cur = chars[read];
+            int count = 0;
+
+            
+            while (read < chars.size() && chars[read] == cur) {
+                read++;
+                count++;
+            }
+
+            
+            chars[write++] = cur;
+
+            
+            if (count > 1) {
+                string s = to_string(count);
+                for (char d : s) {
+                    chars[write++] = d;
+                }
+            }
+        }
+        return write;
+    }
+};
