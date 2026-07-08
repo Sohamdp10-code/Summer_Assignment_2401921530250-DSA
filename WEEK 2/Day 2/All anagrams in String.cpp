8b14146fd8c97c441588c46316a39fbe1477e683
Sohamdp10 <cseaimlbatch28@gmail.com>
2026-07-08 23:50:40 +0530

    Create All anagrams in String.cpp

---
+++ WEEK 2/Day 2/All anagrams in String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        vector<int> pCount(26,0), sCount(26,0);
+
+        for(char c : p)
+         pCount[c-'a']++;
+
+    int k = p.size();
+
+    for(int i=0;i<s.size();i++){
+        sCount[s[i]-'a']++;
+
+        if(i>=k)
+         sCount[s[i-k]-'a']--;
+
+        if(sCount==pCount)
+         ans.push_back(i-k+1);
+    }
+    return ans;
+    }
+    
+};
