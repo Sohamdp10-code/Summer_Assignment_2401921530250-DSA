619bd01dd0235d357552925362445d6cc81b8171
Sohamdp10 <cseaimlbatch28@gmail.com>
2026-07-09 23:12:25 +0530

    Create Palindrome Linked lIst.cpp

---
+++ WEEK 3/DAY 2/Palindrome Linked lIst.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+        ListNode *slow = head, *fast = head;
+
+         while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+      
+          ListNode *prev = NULL;
+
+        while (slow) {
+            ListNode *next = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+          while (prev) {
+            if (head->val != prev->val)
+                return false;
+
+            head = head->next;
+            prev = prev->next;
+        }
+
+        return true;
+    }
+};
+// A palindrome is something that reads the same forward and backward.
