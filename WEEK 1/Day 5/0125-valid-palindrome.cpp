b1b01aeb838a20bce6adbf08227e5894246c316e
Sohamdp10 <cseaimlbatch28@gmail.com>
2026-07-13 20:28:14 +0530

    Add files via upload

---
+++ WEEK 1/Day 5/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        
+        string cleaned = "";
+        for (char c : s) {
+            if (isalnum(c)) {              
+                cleaned += tolower(c);     
+            }
+        }
+        
+        
+        string reversed = cleaned;
+        reverse(reversed.begin(), reversed.end());
+        
+        return cleaned == reversed;
+    }
+};
